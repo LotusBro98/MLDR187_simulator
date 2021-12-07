c7b215daf5bc6a9f70441b11a5ad618eea3a7a9a
Alex <alexpan98@yandex.ru>
2021-12-07 19:24:21 +0300

    implementing instructions

--- core/src/CPU.cpp
+++ core/src/CPU.cpp
@@ -2,6 +2,10 @@
 #include <memory.h>
 #include <Config.h>
 
+#define FIELD(x, base, size) (((x) >> (base)) & ~(((uint32_t)-1) << (size)))
+#define BIT(x, n) (((x) >> (n)) & 1)
+#define SIGN_BIT(b, n) (((uint32_t)-(b)) << (n))
+
 extern void (CPU::*const execute_op[Instruction::COUNT])(Instruction& inst);
 
 CPU::CPU(Memory *memory) {
@@ -36,11 +40,89 @@ uint32_t CPU::fetch() {
 }
 
 void CPU::execute_LUI(Instruction &inst) {
-
+    regs[inst.rd] = inst.immediate;
 }
 
 void CPU::execute_ILL(Instruction &inst) {
     throw std::runtime_error("Illegal instruction");
+}
+
+void CPU::execute_AUIPC(Instruction &inst) {
+    regs[inst.rd] = inst.immediate + pc;
+}
+
+void CPU::execute_JAL(Instruction &inst) {
+    regs[inst.rd] = pc + (4 >> inst.is_c_ext);
+    pc += inst.immediate;
+}
+
+void CPU::execute_JALR(Instruction &inst) {
+    regs[inst.rd] = pc + (4 >> inst.is_c_ext);
+    pc = regs[inst.rs1] + inst.immediate;
+}
+
+void CPU::execute_BNE(Instruction &inst) {
+    if (regs[inst.rs1] != regs[inst.rs2])
+        pc += inst.immediate - (4 >> inst.is_c_ext);
+}
+
+void CPU::execute_BEQ(Instruction &inst) {
+    if (regs[inst.rs1] == regs[inst.rs2])
+        pc += inst.immediate - (4 >> inst.is_c_ext);
+}
+
+void CPU::execute_BLT(Instruction &inst) {
+    if ((int32_t)regs[inst.rs1] < (int32_t)regs[inst.rs2])
+        pc += inst.immediate - (4 >> inst.is_c_ext);
+}
+
+void CPU::execute_BGE(Instruction &inst) {
+    if ((int32_t)regs[inst.rs1] >= (int32_t)regs[inst.rs2])
+        pc += inst.immediate - (4 >> inst.is_c_ext);
+}
+
+void CPU::execute_BLTU(Instruction &inst) {
+    if (regs[inst.rs1] < regs[inst.rs2])
+        pc += inst.immediate - (4 >> inst.is_c_ext);
+}
+
+void CPU::execute_BGEU(Instruction &inst) {
+    if (regs[inst.rs1] >= regs[inst.rs2])
+        pc += inst.immediate - (4 >> inst.is_c_ext);
+}
+
+void CPU::execute_LB(Instruction &inst) {
+    uint32_t byte = memory->read_byte(regs[inst.rs1] + inst.immediate);
+    regs[inst.rd] = byte | SIGN_BIT(BIT(byte, 7), 7);
+}
+
+void CPU::execute_LH(Instruction &inst) {
+    uint32_t half = memory->read_half(regs[inst.rs1] + inst.immediate);
+    regs[inst.rd] = half | SIGN_BIT(BIT(half, 15), 15);
+}
+
+void CPU::execute_LW(Instruction &inst) {
+    regs[inst.rd] = memory->read_word(regs[inst.rs1] + inst.immediate);
+}
+
+void CPU::execute_LBU(Instruction &inst) {
+    regs[inst.rd] = memory->read_byte(regs[inst.rs1] + inst.immediate);
+}
+
+void CPU::execute_LHU(Instruction &inst) {
+    regs[inst.rd] = memory->read_half(regs[inst.rs1] + inst.immediate);
+}
+
+void CPU::execute_SB(Instruction &inst) {
+    memory->write_byte(regs[inst.rs1] + inst.immediate, regs[inst.rs2]);
+}
+
+void CPU::execute_SH(Instruction &inst) {
+    memory->write_half(regs[inst.rs1] + inst.immediate, regs[inst.rs2]);
+}
+
+void CPU::execute_SW(Instruction &inst) {
+    memory->write_word(regs[inst.rs1] + inst.immediate, regs[inst.rs2]);
 }
 
 void (CPU::*const execute_op[Instruction::COUNT])(Instruction& inst) = {
